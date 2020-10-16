085f9fc78029b7d5c34187b9e1a1794c765183a7
Developer <developer@Developers-iMac.local>
2020-10-16 11:04:51 +0300

    feature: create timelog ep [ref:14]

--- src/timelogs/dto/create-timelog.dto.ts
+++ src/timelogs/dto/create-timelog.dto.ts
@@ -1,9 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Types } from 'mongoose';
 
 export class CreateTimelogDto {
-  @ApiProperty() public readonly project!: Types.ObjectId;
-  @ApiProperty() public readonly uid!: Types.ObjectId;
   @ApiProperty() public readonly desc!: string;
   @ApiProperty() public readonly date!: string;
   @ApiProperty() public readonly time!: string;
