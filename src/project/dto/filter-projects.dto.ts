8c8982c04fa0700e2b663ed386beaaf74e75db1a
Developer <developer@Developers-iMac.local>
2020-10-19 16:38:27 +0300

    feature: creating ep 'get rules' [ref:26]

--- src/project/dto/filter-projects.dto.ts
+++ src/project/dto/filter-projects.dto.ts
@@ -1,8 +1,10 @@
+import { ProjectStatus } from './../enums/project-status.enum';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class ProjectFilterDto {
   @ApiPropertyOptional() public readonly uid?: string;
   @ApiPropertyOptional() public readonly onGoing?: string;
   @ApiPropertyOptional() public readonly isInternal?: string;
+  @ApiPropertyOptional() public readonly status?: ProjectStatus;
   @ApiPropertyOptional() public readonly stack?: string;
 }
