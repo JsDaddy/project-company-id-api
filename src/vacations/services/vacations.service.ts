96b9716f952eed7851db9d7e9c7ec31c33ebf057
Developer <developer@Developers-iMac-2.local>
2020-11-18 15:42:07 +0200

    fix: slack notifications [ref: no-ref]

--- src/vacations/services/vacations.service.ts
+++ src/vacations/services/vacations.service.ts
@@ -47,7 +47,7 @@ export class VacationsService {
         this._slackService.sendMessage(
           slack,
           `You have request for vacation (${this.getType(type)}) \n
-          *Date*: ${createVacationDto.date.toLocaleString('en-US', {
+*Date*: ${createVacationDto.date.toLocaleString('en-US', {
             weekday: 'long',
             year: 'numeric',
             month: 'numeric',
