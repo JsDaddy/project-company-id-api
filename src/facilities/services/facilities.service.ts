58ddd0142cca2559be2b9406d76f5ea059250703
Developer <developer@Developers-iMac-2.local>
2020-12-24 11:54:37 +0200

    fix eps get facilities/feedbacks

--- src/facilities/services/facilities.service.ts
+++ src/facilities/services/facilities.service.ts
@@ -1,4 +1,4 @@
-import { IFacilities } from './../interfaces/facilities.interface';
+import { IFacilities } from '../interfaces/facilities.interface';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Document } from 'mongoose';
@@ -9,7 +9,34 @@ export class FacilitiesService {
     private readonly facilitiesModel: Model<IFacilities & Document>,
   ) {}
 
+  // public async findFacility(facility: string): Promise<IFacilities | null> {
+  //   return await this.facilitiesModel.findOne({ name: facility });
+  // }
   public async findFacility(facility: string): Promise<IFacilities | null> {
-    return await this.facilitiesModel.findOne({ name: facility });
+    // tslint:disable-next-line:no-any
+    const service: IFacilities[] = await this.facilitiesModel.aggregate([
+      { $match: { name: facility } },
+      {
+        $lookup: {
+          from: 'stacks',
+          localField: 'stack',
+          as: 'stack',
+          foreignField: '_id',
+        },
+      },
+      {
+        $lookup: {
+          from: 'feedbacks',
+          localField: 'feedback',
+          as: 'feedback',
+          foreignField: '_id',
+        },
+      },
+      {
+        $unwind: '$feedback',
+      },
+    ]);
+    return service[0];
+    // return await this.facilitiesModel.findOne({ name: facility });
   }
 }
